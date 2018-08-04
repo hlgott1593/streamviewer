f1ecb5de55cf76258a2c8b407c63cb4274b90467
Harrison Gottlieb <harrison.gottlieb@rubixis.com>
2018-08-04 19:06:30 +0000

    updating redirect url.

--- streamviewer/settings.py
+++ streamviewer/settings.py
@@ -157,5 +157,5 @@ YOUTUBE_API_SCOPES = [
 ]
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
-REDIRECT_URL='stormy-headland-92009.herokuapp.com/oauth2callback'
+REDIRECT_URL='https://stormy-headland-92009.herokuapp.com/oauth2callback'
 
