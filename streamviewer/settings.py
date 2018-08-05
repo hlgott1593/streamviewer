471baeb722d0830c1d16c32e29045c9d4af39e01
Harrison Gottlieb <harrison.gottlieb@rubixis.com>
2018-08-05 04:38:19 +0000

    updating session engine.

--- streamviewer/settings.py
+++ streamviewer/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+
 ROOT_URLCONF = 'streamviewer.urls'
 
 TEMPLATES = [
