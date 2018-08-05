1c386a0e2125903cb6de3eea7ee2cfe6ef325efd
Harrison Gottlieb <harrison.gottlieb@rubixis.com>
2018-08-05 04:07:29 +0000

    adding print test.

--- api/views.py
+++ api/views.py
@@ -137,6 +137,7 @@ class StreamViewSet(viewsets.ModelViewSet):
 		# return Response(JSONRenderer().render(serializer.data))
 		response = {'status': 'FAILED'}
 		if not request.session.get('credentials'):
+			print(request.session)
 			return JsonResponse(response)
 
 		youtube = Utils.getYouTubeAPI(request)
