534a59f9178a2ce7bfbcba3ea79bbeb5992fe19b
Harrison Gottlieb <harrison.gottlieb@rubixis.com>
2018-08-06 03:30:32 +0000

    increasing message model primary key size.

--- api/models.py
+++ api/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 # Message Model
 class Message(models.Model):
-	messageId = models.CharField(max_length=100, primary_key=True)
+	messageId = models.CharField(max_length=200, primary_key=True)
 	username = models.TextField(max_length=50)
 	liveChatId = models.TextField(max_length=200)
 	text = models.TextField()
